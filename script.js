8d7667983676e53b5394e0d6ee687da9c7b2e83f
rafael ranzolin <147657601+rafaelranzolin@users.noreply.github.com>
2025-03-11 13:29:46 -0300

    .

--- script.js
+++ script.js
@@ -22,3 +22,7 @@ botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
+   const alternaContraste = document.getElementById('alterna-contraste')
+   alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
