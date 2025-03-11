ea3b502ac620f35e426142fdb7cc54b04f13bced
rafael ranzolin <147657601+rafaelranzolin@users.noreply.github.com>
2025-03-11 13:21:29 -0300

    .

--- script.js
+++ script.js
@@ -16,3 +16,9 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
 });
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
